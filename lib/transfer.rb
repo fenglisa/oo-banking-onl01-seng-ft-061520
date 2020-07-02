42bbd4778142d03746cfdc7b1d4318f78a9904b5
Lisa Feng <lisafeng10@gmail.com>
2020-07-02 02:47:30 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -24,6 +24,7 @@ class Transfer
       receiver.balance += amount
       self.status = "complete"
     else
+      self.status = "rejected"
       return "Transaction rejected. Please check your account balance."
     end
   end
