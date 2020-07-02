807fc5addbd5ccd580e6c0f79e8ed2981a3fd08f
Lisa Feng <lisafeng10@gmail.com>
2020-07-02 02:26:00 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,9 +1,9 @@
 class Transfer
-  attr_accessor :status
+  attr_accessor :status, :sender
   attr_writer
   attr_reader
   
-  def initialize()
+  def initialize(sender)
     @status = status
   end
 end
