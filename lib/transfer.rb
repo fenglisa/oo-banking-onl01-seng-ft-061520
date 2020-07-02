f3f0734138d2bbd123f5d2a41b4c3e74ed89490f
Lisa Feng <lisafeng10@gmail.com>
2020-07-02 03:02:58 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -19,7 +19,7 @@ class Transfer
   end
   
   def execute_transaction
-    if self.valid? && self.status == "pending"
+    if self.valid? && sender.balance>self.amount &&self.status == "pending"
       sender.balance -= amount
       receiver.balance += amount
       self.status = "complete"
