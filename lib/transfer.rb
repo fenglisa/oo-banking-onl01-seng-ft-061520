0dea601e1b17dd7dae9bd4f2da75666adebb319d
Lisa Feng <lisafeng10@gmail.com>
2020-07-02 03:07:30 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -30,7 +30,7 @@ class Transfer
   end
   
   def reverse_transfer
-    if self.execute_transaction.status = "complete"
+    if self.status = "complete"
       sender.balance += amount
       receiver.balance -= amount
       self.status = "reversed"
