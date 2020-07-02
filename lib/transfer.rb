f0116cea672f9ddd366d9d0740b39795b00b522d
Lisa Feng <lisafeng10@gmail.com>
2020-07-02 02:27:36 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,10 +1,11 @@
 class Transfer
-  attr_accessor :sender, :receiver
+  attr_accessor :sender, :receiver, :status
   attr_writer
   attr_reader
   
-  def initialize(sender, receiver)
+  def initialize(sender, receiver, status)
     @sender = sender
     @receiver = receiver
+    @status = "pending"
   end
 end
